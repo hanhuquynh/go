1b742ce55991abe7599fdfb996d77698701e0c94
Quỳnh <hanhuquynh25122001@gmail.com>
2022-06-17 09:18:37 +0700

    exercise3

--- main.go
+++ main.go
@@ -1,5 +1,25 @@
 package main
 
-func main() {
+import (
+	"log"
+	"sync"
+	"time"
+)
 
+var wg sync.WaitGroup
+
+func chanRoutine() {
+	wg.Add(1)
+	log.Print("hello 1")
+	go func() {
+		time.Sleep(1 * time.Second)
+		log.Print("hello 3")
+		wg.Done()
+	}()
+	log.Print("hello 2")
+	wg.Wait()
+}
+
+func main() {
+	chanRoutine()
 }

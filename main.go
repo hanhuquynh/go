b0b134b14ef2b5c7dfe122d5b5d73b5832586fa2
Quỳnh <hanhuquynh25122001@gmail.com>
2022-06-19 23:05:04 +0700

    exercise3

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"log"
+	"strconv"
 	"sync"
 	"time"
 )
@@ -25,4 +26,19 @@ func main() {
 	// Bài 1"
 	fmt.Println("Bài 1:")
 	chanRoutine()
+	fmt.Println("-----------------------------------------------")
+
+	// Bài 2:
+	fmt.Println("Bài 2:")
+	X := make(map[string]string)
+
+	for i := 1; i <= 1000; i++ {
+		X["Key "+strconv.Itoa(i)] = "Value " + strconv.Itoa(i)
+	}
+	for key, value := range X {
+		log.Printf("%v: %v", key, value)
+	}
+
+	log.Print("Length: ", len(X))
+
 }

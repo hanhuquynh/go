3cae09a8d26dfda54d1169e181b671886febd03b
Quỳnh <hanhuquynh25122001@gmail.com>
2022-06-22 23:28:52 +0700

    exercise3

--- main.go
+++ main.go
@@ -1,14 +1,19 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"log"
+	"os"
 	"strconv"
 	"sync"
 	"time"
 )
 
-var wg sync.WaitGroup
+var (
+	wg sync.WaitGroup
+	mu sync.Mutex
+)
 
 func chanRoutine() {
 	wg.Add(1)
@@ -23,6 +28,7 @@ func chanRoutine() {
 }
 
 func main() {
+
 	// Bài 1"
 	fmt.Println("Bài 1:")
 	chanRoutine()
@@ -32,13 +38,57 @@ func main() {
 	fmt.Println("Bài 2:")
 	X := make(map[string]string)
 
-	for i := 1; i <= 1000; i++ {
-		X["Key "+strconv.Itoa(i)] = "Value " + strconv.Itoa(i)
-	}
+	wg.Add(3)
+
+	go func() {
+		mu.Lock()
+		for i := 1; i <= 1000; i++ {
+			X["Key i "+strconv.Itoa(i)] = "Value " + strconv.Itoa(i)
+		}
+		mu.Unlock()
+		wg.Done()
+	}()
+
+	go func() {
+		mu.Lock()
+		for j := 1; j <= 1000; j++ {
+			X["Key j "+strconv.Itoa(j)] = "Value " + strconv.Itoa(j)
+		}
+		mu.Unlock()
+		wg.Done()
+	}()
+
+	go func() {
+		mu.Lock()
+		for k := 1; k <= 1000; k++ {
+			X["Key k "+strconv.Itoa(k)] = "Value " + strconv.Itoa(k)
+		}
+		mu.Unlock()
+		wg.Done()
+	}()
+	wg.Wait()
+
 	for key, value := range X {
-		log.Printf("%v: %v", key, value)
+		fmt.Printf("%v: %v\n", key, value)
 	}
 
-	log.Print("Length: ", len(X))
+	fmt.Println("Length X: ", len(X))
+
+	fmt.Println("-----------------------------------------------")
+
+	// Bài 4:
+	fmt.Println("Bài 4:")
+	file, err := os.Open("file.txt")
+	if err != nil {
+		log.Print(err)
+	}
+
+	fileScanner := bufio.NewScanner(file)
+
+	for fileScanner.Scan() {
+		fmt.Println(fileScanner.Text())
+	}
+
+	file.Close()
 
 }
